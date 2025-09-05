444e04934f775764b47827637f9bf3bb415ffd16
Shianm <giankarloy@gmail.com>
2025-09-05 10:54:21 -0300

    arreglo tp parque

--- Semana2/proyecto_parque/gestion_parque/parque.py
+++ Semana2/proyecto_parque/gestion_parque/parque.py
@@ -32,3 +32,33 @@ def registrar_visita():
 
     mostrar_atracciones()
     cantidad = int(input("Cuantas atracciones quiere usar?: "))
+
+    costo_total = 0
+    atracciones_texto = ""
+
+    i = 1
+    while i <= cantidad:
+        opcion = int(input("Elija la atraccion " + str(i) + " (1-3): "))
+
+        if puede_subir(edad, opcion):
+            print("Puede subir a la atraccion", opcion)
+            costo_total += calcular_precio(opcion)
+            atracciones_texto += str(opcion) + " "
+        else:
+            print("No puede subir a la atracción", opcion, "por su edad")
+
+        i += 1
+
+    if cantidad >= 3:
+        costo_total = costo_total * 0.9
+        print("Se aplico un descuento del 10% por comprar 3 o mas atracciones")
+
+
+    return nombre, edad, atracciones_texto, costo_total
+
+
+def mostrar_resumen(nombre, edad, atracciones_texto, costo_total):
+    print("Visitante:", nombre)
+    print("Edad:", edad)
+    print("Atracciones elegidas:", atracciones_texto)
+    print("Costo total a pagar: $", costo_total)
--- Semana2/proyecto_parque/main.py
+++ Semana2/proyecto_parque/main.py
@@ -1,2 +1,9 @@
 from gestion_parque.parque import *
 
+def main():
+    nombre, edad, atracciones_texto, costo_total = registrar_visita()
+
+
+    mostrar_resumen(nombre, edad, atracciones_texto, costo_total)
+
+main()
